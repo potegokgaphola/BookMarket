459e6236cc6b063b0503fc7496754db931385a4f
Potego Kgaphola <seshupu@bbd.co.za>
2020-11-17 09:57:42 +0200

    set up authentication

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var engine = require('consolidate');
 
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname, '/public');
+app.engine('html', engine.mustache);
+app.set('view engine', 'html');
+// app.get('/', function(req, res) {
+//     res.render('index')
+// })
+
+app.get('/home', function(req, res) {
+    res.render('login')
+})
 
 app.post('/signup', function(req, res) {
     console.log(JSON.stringify({
--- public/index.js
+++ public/index.js
@@ -17,10 +17,41 @@ $("#signup-a-btn").click(function(event) {
     sessionStorage.setItem('lastform', 'signup');
 })
 
-$("#signup-btn").click(function(event) {
+$("#login-btn").click(async function(event) {
     event.preventDefault();
-    showLoginForm();
-    sessionStorage.setItem('lastform', 'login');
+    const email = document.getElementById('login-username');
+    const password = document.getElementById('login-password');
+
+    try {
+        await auth.signInWithEmailAndPassword(email.value, password.value);
+        $.ajax({
+            type: 'GET',
+            url: '/home',
+            success: function() {
+                console.info(`User ${email.value} has logged in`);
+            }
+        })
+    } catch (error) {
+        console.error(error);
+    } 
+})
+
+$("#signup-btn").click(async function(event) {
+    event.preventDefault();
+    const firstname = document.getElementById('firstname');
+    const lastname = document.getElementById('lastname');
+    const email = document.getElementById('signp-username');
+    const password = document.getElementById('signup-password');
+    const confirmpassword = document.getElementById('confirm-password');
+
+    try {
+        await auth.createUserWithEmailAndPassword(email.value, password.value);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        showLoginForm();
+        sessionStorage.setItem('lastform', 'login');
+    }
 })
 
 function showSignupForm() {
@@ -32,3 +63,25 @@ function showLoginForm() {
     $('#signup-form').addClass('hidden');
     $('#login-form').removeClass('hidden');
 }
+
+function signOut() {
+    auth.signOut();
+}
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+apiKey: "AIzaSyDaZPkHZHF07NxYfmTxtnKi0t0N4ozaGlg",
+authDomain: "bookmarket-b87b9.firebaseapp.com",
+databaseURL: "https://bookmarket-b87b9.firebaseio.com",
+projectId: "bookmarket-b87b9",
+storageBucket: "bookmarket-b87b9.appspot.com",
+messagingSenderId: "67210480007",
+appId: "1:67210480007:web:385b1c328dc0ec4966eb22",
+measurementId: "G-02HGWR23T6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+const auth = firebase.auth();
